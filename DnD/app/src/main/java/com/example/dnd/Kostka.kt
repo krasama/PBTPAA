3acd39995620f56a35845ecb0e6c640d2b86c429
krasama <krasa.matej@gmail.com>
2021-05-04 11:57:18 +0200

    generace_img_kostek

--- DnD/app/src/main/java/com/example/dnd/Kostka.kt
+++ DnD/app/src/main/java/com/example/dnd/Kostka.kt
@@ -5,20 +5,19 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.hod_kostkou.*
 
-//import kotlinx.android.synthetic.main.hod_kostkou*
-
 class Kostka: AppCompatActivity() {
+    var stena: Int = 6
+    var pocet: Int = 1
+    private var vysledek: Int =0
 
-    lateinit var dice_image: ImageView
+    lateinit var diceImage: ImageView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hod_kostkou)
-            // TODO: 22.03.2021
 
-            var stena: Int = 6
-            var pocet: Int = 1
+
 
             jedna.setOnClickListener{
                 pocet = 1
@@ -58,7 +57,8 @@ class Kostka: AppCompatActivity() {
 
             start.setOnClickListener{
 
-                var vysledek = hod(pocet, stena)
+               vysledek = hod(pocet, stena)
+                vykres(vysledek)
 
             }
 
@@ -69,21 +69,23 @@ class Kostka: AppCompatActivity() {
 
         }
 
-        fun hod(pocet:Int, stena: Int) {
-            var vysledek: Int = 0
+        private fun hod(pocet:Int, stena: Int): Int {
+            var vysledekH: Int = 0
             for (i in 1 .. pocet){
-                vysledek = rnd(stena)
-                //return vysledek
+                vysledekH = rnd(stena)
             }
+            return vysledekH
+        }
+            private fun vykres(vysledek:Int){
                 if (stena ==4){
                     val drawableResources = when(vysledek){
-                        1->R.drawable.d6_1
-                        2->R.drawable.d6_2
-                        3->R.drawable.d6_3
-                        else -> R.drawable.d6_4
+                        1->R.drawable.d4_1
+                        2->R.drawable.d4_2
+                        3->R.drawable.d4_3
+                        else -> R.drawable.d4_4
 
                     }
-                    dice_image.setImageResource(drawableResources)
+                    diceImage.setImageResource(drawableResources)
                 }
                 else if (stena == 6) {
                     val drawableResources = when(vysledek){
@@ -95,89 +97,103 @@ class Kostka: AppCompatActivity() {
                         else -> R.drawable.d6_6
 
                     }
-                    dice_image.setImageResource(drawableResources)
+                    diceImage.setImageResource(drawableResources)
                 }
                 else if (stena == 8){
                     val drawableResources = when(vysledek){
-                        1->R.drawable.d6_1
-                        2->R.drawable.d6_2
-                        3->R.drawable.d6_3
-                        4->R.drawable.d6_4
-                        5->R.drawable.d6_5
-                        6->R.drawable.d6_5
-                        7->R.drawable.d6_5
-                        else -> R.drawable.d6_6
+                        1->R.drawable.d4_1
+                        2->R.drawable.d4_2
+                        3->R.drawable.d4_3
+                        4->R.drawable.d4_4
+                        5->R.drawable.d8_5
+                        6->R.drawable.d8_6
+                        7->R.drawable.d8_7
+                        else -> R.drawable.d8_8
 
                     }
-                    dice_image.setImageResource(drawableResources)
+                    diceImage.setImageResource(drawableResources)
                 }
                 else if (stena == 10){
                     val drawableResources = when(vysledek){
-                        1->R.drawable.d6_1
-                        2->R.drawable.d6_2
-                        3->R.drawable.d6_3
-                        4->R.drawable.d6_4
-                        5->R.drawable.d6_5
-                        6->R.drawable.d6_5
-                        7->R.drawable.d6_5
-                        8->R.drawable.d6_5
-                        9->R.drawable.d6_5
-                        else -> R.drawable.d6_6
+                        1->R.drawable.d10_0
+                        2->R.drawable.d10_1
+                        3->R.drawable.d10_2
+                        4->R.drawable.d10_3
+                        5->R.drawable.d10_4
+                        6->R.drawable.d10_5
+                        7->R.drawable.d10_6
+                        8->R.drawable.d10_7
+                        9->R.drawable.d10_8
+                        else -> R.drawable.d10_9
 
                     }
-                    dice_image.setImageResource(drawableResources)
+                    diceImage.setImageResource(drawableResources)
                 }
                 else if (stena == 12){
                     val drawableResources = when(vysledek){
-                        1->R.drawable.d6_1
-                        2->R.drawable.d6_2
-                        3->R.drawable.d6_3
-                        4->R.drawable.d6_4
-                        5->R.drawable.d6_5
-                        6->R.drawable.d6_5
-                        7->R.drawable.d6_5
-                        8->R.drawable.d6_5
-                        9->R.drawable.d6_5
-                        10->R.drawable.d6_5
-                        11->R.drawable.d6_5
-                        else -> R.drawable.d6_6
+                        1->R.drawable.d12_1
+                        2->R.drawable.d12_2
+                        3->R.drawable.d12_3
+                        4->R.drawable.d12_4
+                        5->R.drawable.d12_5
+                        6->R.drawable.d12_6
+                        7->R.drawable.d12_7
+                        8->R.drawable.d12_8
+                        9->R.drawable.d12_9
+                        10->R.drawable.d12_10
+                        11->R.drawable.d12_11
+                        else -> R.drawable.d12_12
 
                     }
-                    dice_image.setImageResource(drawableResources)
+                    diceImage.setImageResource(drawableResources)
                 }
                 else if (stena == 20){
                     val drawableResources = when(vysledek){
-                        1->R.drawable.d6_1
-                        2->R.drawable.d6_2
-                        3->R.drawable.d6_3
-                        4->R.drawable.d6_4
-                        5->R.drawable.d6_5
-                        6->R.drawable.d6_5
-                        7->R.drawable.d6_5
-                        8->R.drawable.d6_5
-                        9->R.drawable.d6_5
-                        10->R.drawable.d6_5
-                        11->R.drawable.d6_5
-                        12->R.drawable.d6_5
-                        13->R.drawable.d6_5
-                        14->R.drawable.d6_5
-                        15->R.drawable.d6_5
-                        16->R.drawable.d6_5
-                        17->R.drawable.d6_5
-                        18->R.drawable.d6_5
-                        19->R.drawable.d6_5
-                        else -> R.drawable.d6_6
+                        1->R.drawable.d4_1
+                        2->R.drawable.d4_2
+                        3->R.drawable.d4_3
+                        4->R.drawable.d4_4
+                        5->R.drawable.d8_5
+                        6->R.drawable.d8_6
+                        7->R.drawable.d8_7
+                        8->R.drawable.d8_8
+                        9->R.drawable.d20_9
+                        10->R.drawable.d20_10
+                        11->R.drawable.d20_11
+                        12->R.drawable.d20_12
+                        13->R.drawable.d20_13
+                        14->R.drawable.d20_14
+                        15->R.drawable.d20_15
+                        16->R.drawable.d20_16
+                        17->R.drawable.d20_17
+                        18->R.drawable.d20_18
+                        19->R.drawable.d20_19
+                        else -> R.drawable.d20_20
 
                     }
-                    dice_image.setImageResource(drawableResources)
+                    diceImage.setImageResource(drawableResources)
                 }
                 else{
-                    // TODO: 26.04.2021
-                }
+                    val drawableResources = when(vysledek){
+                        (0-4)->R.drawable.d10_00
+                        (5-14)->R.drawable.d10_10
+                        (15-24)->R.drawable.d10_20
+                        (25-34)->R.drawable.d10_30
+                        (35-44)->R.drawable.d10_40
+                        (45-54)->R.drawable.d10_50
+                        (55-64)->R.drawable.d10_60
+                        (65-74)->R.drawable.d10_70
+                        (75-84)->R.drawable.d10_80
+                        (85-94)->R.drawable.d10_90
+                        else -> R.drawable.d10_00
 
+                    }
+                    diceImage.setImageResource(drawableResources)
+                }
         }
 
-        fun rnd(stena: Int):Int{
+
+        private fun rnd(stena: Int):Int{
 
             return (1..stena).shuffled().first()
         }
--- DnD/app/src/main/java/com/example/dnd/Postava.kt
+++ DnD/app/src/main/java/com/example/dnd/Postava.kt
@@ -5,6 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.generace_postavy.*
 
 class Postava : AppCompatActivity(){
+
+        var dexterity: String = ""
+        var strength: String = ""
+        var intelligence: String = ""
+        var wisdom: String = ""
+        var stamina: String = ""
+        var charisma: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.generace_postavy)
@@ -12,6 +20,41 @@ class Postava : AppCompatActivity(){
         generace_zpet.setOnClickListener {
             this.setContentView(R.layout.activity_main)
         }
+
+        generace.setOnClickListener {
+            generovat()
+            vypis()
+        }
+
+        reset_generace.setOnClickListener {
+            text_dexterity.text = ""
+            text_strength.text = ""
+            text_intelligence.text = ""
+            text_wisdom.text = ""
+            text_stamina.text = ""
+            text_charisma.text = ""
+        }
     }
-// TODO: 22.03.2021
+        private fun generovat(){
+            dexterity = rnd().toString()
+            strength = rnd().toString()
+            intelligence = rnd().toString()
+            wisdom = rnd().toString()
+            stamina = rnd().toString()
+            charisma = rnd().toString()
+        }
+
+        private fun rnd():Int{
+
+            return (1..18).shuffled().first()
+        }
+
+        private fun vypis(){
+            text_dexterity.text = dexterity
+            text_strength.text = strength
+            text_intelligence.text = intelligence
+            text_wisdom.text = wisdom
+            text_stamina.text = stamina
+            text_charisma.text = charisma
+        }
 }
